4a9800186bc8f3a3a07a2a52213891bba00d899d
kska0221 <aksinia.ra@gmail.com>
2025-06-10 20:58:59 +0500

    Homework

--- Module/src/main/java/org/example/Task1.java
+++ Module/src/main/java/org/example/Task1.java
@@ -5,11 +5,10 @@ import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
-        System.out.printf("Введите длину забора (в сантиметрах): ");
+        System.out.print("Введите длину забора (в сантиметрах): ");
 
-        try {
             Scanner scanner = new Scanner(System.in);
-            int fenceLength = scanner.nextInt();
+            double fenceLength = scanner.nextDouble();
 
             // 15 букв
             int numberLetters = 15;
@@ -23,11 +22,8 @@ public class Task1 {
             // 12 см на каждый пробел
             int letterSpace = 12;
 
-            if ((numberLetters / 3 * letterLength) + (numberSpaces * letterSpace) > fenceLength)
+            if (((double) numberLetters / 3 * letterLength) + (numberSpaces * letterSpace) > fenceLength)
                 System.out.println("Надпись не поместится на забор!");
             else System.out.println("Надпись поместится на забор.");
-        } catch (InputMismatchException exception) {
-            System.out.println("Введите целое число!");
-        }
     }
 }
